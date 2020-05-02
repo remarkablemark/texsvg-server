de5db13a254b27cea6dcdc1b8542a0b5ccfbf2a5
Mark <mark@remarkablemark.org>
2020-05-02 13:43:07 -0400

    feat(index): add 404 route

--- index.js
+++ index.js
@@ -21,6 +21,10 @@ app.get('/heartbeat', (req, res, next) => {
   res.send('OK');
 });
 
+app.use((req, res, next) => {
+  res.status(404).send('404');
+});
+
 const port = process.env.port || 3000;
 const hostname = process.env.HOST;
 app.listen(port, hostname, () =>
