058458bee4536c80d3bffed6299f63c821381a7d
Mark <mark@remarkablemark.org>
2020-05-02 19:07:51 -0400

    feat(index): use morgan to log all requests to stdout
    Using pre-defined format `combined`, which is the standard Apache
    combined log output.

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const compression = require('compression');
 const express = require('express');
+const morgan = require('morgan');
 const routes = require('./routes');
 
 const app = express();
+
 app.disable('x-powered-by');
+app.use(morgan('combined'));
 app.use(compression());
 app.use(routes);
 
