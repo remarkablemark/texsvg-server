991d4598bae863b02e44d2b254c362afe85eee6d
Mark <mark@remarkablemark.org>
2020-05-02 19:15:59 -0400

    feat(index): use 'dev' output format for morgan during development

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const compression = require('compression');
 const express = require('express');
 const morgan = require('morgan');
+const { isDevelopment } = require('./constants');
 const routes = require('./routes');
 
 const app = express();
 
 app.disable('x-powered-by');
-app.use(morgan('combined'));
+app.use(morgan(isDevelopment ? 'dev' : 'combined'));
 app.use(compression());
 app.use(routes);
 
