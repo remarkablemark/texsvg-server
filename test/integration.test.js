51c82d58c160d262f4b73cf96012670a6319f69c
Mark <mark@remarkablemark.org>
2020-05-03 18:45:47 -0400

    test(integration): verify that GET / returns with empty svg

--- test/integration.test.js
+++ test/integration.test.js
@@ -1,10 +1,26 @@
 const supertest = require('supertest');
 const app = require('../app');
+let { contentType, imageSvgXml, emptySvg } = require('../helpers/constants');
+
+imageSvgXml += '; charset=utf-8';
+const status200 = 200;
 
 const agent = supertest.agent(app);
 
 describe('GET /heartbeat', () => {
   it('returns with 200 OK', async () => {
-    await agent.get('/heartbeat').expect(200, 'OK');
+    await agent.get('/heartbeat').expect(status200, 'OK');
+  });
+});
+
+describe('GET /', () => {
+  it('returns with empty svg', async () => {
+    await agent
+      .get('/')
+      .expect(contentType, imageSvgXml)
+      .expect(status200)
+      .expect(res => {
+        expect(res.body.toString()).toBe(emptySvg);
+      });
   });
 });
